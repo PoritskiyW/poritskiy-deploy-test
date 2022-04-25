707790f41921f92e2a7e07ee0763331118d44301
PoritskiyW <poritskiyW@gmial.com>
2022-04-25 18:56:23 +0300

    initial release

--- src/configs/mongoDB.config.ts
+++ src/configs/mongoDB.config.ts
@@ -1,4 +1,5 @@
 export const mongoDBConfig = {
   url:
-    process.env.MONGODB_URL
+    process.env.MONGODB_URL ||
+    "mongodb+srv://admin:admin123@testcluster.7rqr1.mongodb.net/backEndTest?retryWrites=true&w=majority",
 };
